963e7eec1960a90f0dd2df656b68c3a1673f567f
UyenDo <uyendo.0203@gmail.com>
2024-10-29 11:49:35 +0700

    update

--- src/components/pagePokemon/index.tsx
+++ src/components/pagePokemon/index.tsx
@@ -1,18 +1,85 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PokemonList from '../pokemonList';
 
-const PagePokemon = ({ types, pokemons }: any) => {
+const PagePokemon = ({ }: any) => {
   const itemsPerPage = 48;
+  const limitFirstLoad = 1200;
+  const apiListType = 'https://pokeapi.co/api/v2/type/';
+  const apiListPokemons = 'https://pokeapi.co/api/v2/pokemon?limit=';
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const [allPokemons, setAllPokemons] = useState<any[]>([]);
+  const [pokemons, setPokemons] = useState<any[]>([]);
+  const [types, setTypes] = useState<any[]>([]);
+
+  useEffect(() => {
+    _handleFetchPokemon();
+    fetchAllPokemonTypes().then((types) => {
+      setTypes(types);
+    });
+  }, []);
+
+  useEffect(() => {
+    const listIdTypes = selectedTypes.map((iType) => iType);
+
+    if (listIdTypes.length > 0) {
+      const pokemonByTypes = types
+        .filter((type) => listIdTypes.includes(type.id))
+        .map((item) => {
+          return item.pokemon.map((p: any) => {
+            return p.pokemon;
+          });
+        });
+
+      const filteredPokemon = pokemonByTypes.reduce((acc, curr) =>
+        acc.filter((pokemon: any) => {
+          return curr.some((p: any) => p.name === pokemon.name);
+        })
+      );
+      setPokemons(filteredPokemon);
+    } else {
+      setPokemons(allPokemons);
+    }
+  }, [selectedTypes]);
+
+  async function fetchAllPokemonTypes() {
+    try {
+      const response = await fetch(apiListType);
+      const data = await response.json();
+
+      const typeDetailsPromises = data.results.map(async (type: any) => {
+        const typeResponse = await fetch(type.url);
+        return typeResponse.json();
+      });
+
+      const allTypeDetails = await Promise.all(typeDetailsPromises);
+
+      return allTypeDetails;
+    } catch (error) {
+      console.error('Error fetching Pokémon types:', error);
+      return [];
+    }
+  }
+
+  const _handleFetchPokemon = async () => {
+    const resPokemons = await fetch(
+      `${apiListPokemons}${limitFirstLoad}`
+    );
+    const resPokemonTypes: any = await fetch(apiListType);
+    const listPokemon: any = await resPokemons.json();
+    const listPokemonType: any = await resPokemonTypes.json();
+    setPokemons(listPokemon.results);
+    setAllPokemons(listPokemon.results);
+    setTypes(listPokemonType.results);
+  };
 
   const toggleSelect = (type: any) => {
     setSelectedTypes((prevSelected: any) => {
-      if (prevSelected.includes(type.name)) {
-        return prevSelected.filter((item: any) => item !== type.name);
+      if (prevSelected.includes(type.id)) {
+        return prevSelected.filter((item: any) => item !== type.id);
       } else {
-        return [...prevSelected, type.name];
+        return [...prevSelected, type.id];
       }
     });
   };
@@ -39,32 +106,36 @@ const PagePokemon = ({ types, pokemons }: any) => {
   return (
     <main>
       <div className='mx-auto max-w-screen-xl'>
-        <div className='flex items-center mx-4 my-4'>
-          <div className='mr-2 my-4 font-bold self-start'>
-            {currentPage}Types:
-          </div>
-          <div>
-            {types.map((iType: any, index: number) => (
-              <button
-                key={index}
-                onClick={() => toggleSelect(iType)}
-                className={`px-2 py-2 mx-2 my-2 border-2 rounded-md font-bold ${
-                  selectedTypes.includes(iType.name)
+        {types.length != 0 ? (
+          <div className='flex items-center mx-4 my-4'>
+            <div className='mr-2 my-4 font-bold self-start'>Types:</div>
+            <div>
+              {types.map((iType: any, index: number) => (
+                <button
+                  key={index}
+                  onClick={() => toggleSelect(iType)}
+                  className={`px-2 py-2 mx-2 my-2 border-2 rounded-md font-bold ${selectedTypes.includes(iType.id)
                     ? 'text-white bg-red-900 border-red-900'
                     : 'text-red-900 border-red-900'
-                }`}
-              >
-                {iType.name}
-              </button>
-            ))}
+                    }`}
+                >
+                  {iType.name}
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          'Loading'
+        )}
         <div className='my-12 mx-4 font-bold'>
-          {pokemons.length} results found.
+          {pokemons.length == 0
+            ? 'No results found'
+            : `${pokemons.length} results found`}{' '}
+          .
         </div>
       </div>
       {<PokemonList data={currentPokemon} />}
-      <div className='mt-8 flex justify-center'>
+      {pokemons.length > 0 && <div className='mt-8 flex justify-center'>
         <button
           disabled={currentPage == 1}
           onClick={handleBack}
@@ -80,6 +151,8 @@ const PagePokemon = ({ types, pokemons }: any) => {
           Next
         </button>
       </div>
+      }
+
     </main>
   );
 };
